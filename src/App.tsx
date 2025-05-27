36ed7e21f2db7bbf9827aab3171f92c5d732b535
Jan Smrcka <jan.smrck@gmail.com>
2025-05-27 13:58:35 +0200

    define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import { TodosProvider } from './providers/todos.provider'
 import { Layout } from './components/layout'
+import TodoListPage from './pages/todo-list.page'
 
 function App() {
   return (
     <Layout>
       <TodosProvider>
-        <BrowserRouter></BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<TodoListPage />} />
+            <Route path="*" element={<div>Not found</div>} />
+          </Routes>
+        </BrowserRouter>
       </TodosProvider>
     </Layout>
   )
