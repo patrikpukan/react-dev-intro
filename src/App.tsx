191e5ccc91ebf3dc004b5617a7e48690eeb879e7
Jan Smrcka <jan.smrck@gmail.com>
2025-05-27 14:55:31 +0200

    react query for detail page

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,42 @@ import { TodosProvider } from './providers/todos.provider'
 import { Layout } from './components/layout'
 import { lazy, Suspense } from 'react'
 import { Spinner } from './components/spinner'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const TodoDetailPage = lazy(() => import('./pages/todo-detail.page'))
 const TodoListPage = lazy(() => import('./pages/todo-list.page'))
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <Layout>
-      <TodosProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Suspense fallback={<div>is loading</div>}>
-                  <TodoListPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/todos/:id"
-              element={
-                <Suspense fallback={<Spinner />}>
-                  <TodoDetailPage />
-                </Suspense>
-              }
-            />
-            <Route path="*" element={<div>Not found</div>} />
-          </Routes>
-        </BrowserRouter>
-      </TodosProvider>
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <TodosProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Suspense fallback={<div>is loading</div>}>
+                    <TodoListPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="/todos/:id"
+                element={
+                  <Suspense fallback={<Spinner />}>
+                    <TodoDetailPage />
+                  </Suspense>
+                }
+              />
+              <Route path="*" element={<div>Not found</div>} />
+            </Routes>
+          </BrowserRouter>
+        </TodosProvider>
+      </Layout>
+    </QueryClientProvider>
   )
 }
 
